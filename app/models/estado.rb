ea3553686cf0dee37607c3c277eabec083b93d66
Guilherme Baufaker Rêgo <gbre.111@gmail.com>
2015-08-19 14:10:11 -0300

    Dados no mapa

---
+++ app/models/estado.rb
@@ -0,0 +1,11 @@
+class Estado
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :sigla, type: String
+  field :nome, type: String
+  field :shape_description, type: String
+  field :has_circle, type: Boolean, default: false
+  field :text_position, type: String
+  field :circle_shape, type: String
+end
