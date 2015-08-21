665cb22e96b0022f6dcf3cd45cd013bf7ba7ced4
Guilherme Baufaker Rêgo <gbre.111@gmail.com>
2015-08-21 10:14:53 -0300

    Main Page Correction

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
   resources :dashboards
-  root to: 'pages#main'
+  root :to => "dashboards#show", :id => Dashboard.last.id
+  # root to: dashboard_path(Dashboard.first)
+
 end
